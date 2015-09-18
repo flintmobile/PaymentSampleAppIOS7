912cb81b6ce3071b217d1723011565759414606a
PC Nguyen <pc@flint.com>
2015-09-18 10:12:28 -0700

    Initial Commit

---
+++ PaymentSampleAppiOS7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaymentSampleAppiOS7
+//
+//  Created by PC on 9/18/15.
+//  Copyright (c) 2015 Flint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaymentSampleAppiOS7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaymentSampleAppiOS7
+//
+//  Created by PC on 9/18/15.
+//  Copyright (c) 2015 Flint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
